50f0fe52eb29278a693e151374a5c616cfdb7c86
manu00-droid <manavsidana00@gmail.com>
2023-12-18 08:42:40 +0530

    fix: converted list of parameters into dict of parameters

--- TestProj/APP/Modules/WeatherForecast.py
+++ TestProj/APP/Modules/WeatherForecast.py
@@ -1,17 +1,17 @@
 import requests
-import Prediction
+
 
 def get_weather(city):
     url = f"http://api.weatherapi.com/v1/current.json?key=&q=${city}&aqi=no`;"
     response = requests.get(url)
     data = response.json()
     if response.status_code == 200:
-        barometer=data['current']['pressure_mb']
-        temp=data['current']['temp_c']
-        wind=data['current']['wind_kph']
-        humidity=data['current']['humidity']
-        weather=data['current']['condition']['text']
-        list=[barometer, temp, wind, humidity, weather]
-        return list
+        barometer = data['current']['pressure_mb']
+        temp = data['current']['temp_c']
+        wind = data['current']['wind_kph']
+        humidity = data['current']['humidity']
+        weather = data['current']['condition']['text']
+        parameters = {"barometer": barometer, "temp": temp, "wind": wind, "humidity": humidity, "weather": weather}
+        return parameters
     else:
-        return "Error occured"
+        return "Error occurred"
