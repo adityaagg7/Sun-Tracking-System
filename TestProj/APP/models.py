1659455e37ea3565216ff5c33b0320022188c27f
adityaagg7 <79642133+adityaagg7@users.noreply.github.com>
2023-12-18 11:42:38 +0530

    feat: added DataBase to store Dynamic voltages

--- TestProj/APP/models.py
+++ TestProj/APP/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+class Voltage(models.Model):
+    voltage = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=255)
+#     slug = models.SlugField(unique=True, max_length=255)
+#     content = models.TextField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     author = models.TextField()
--- TestProj/APP/views.py
+++ TestProj/APP/views.py
@@ -3,21 +3,25 @@ from rest_framework.decorators import api_view
 import random
 from Modules import WeatherForecast
 from Modules import Prediction
-
+from APP.models import Voltage
 
 @api_view(['GET', 'POST'])
 def receiveESPData(request):
+    v=0
     if request.method == 'POST':
-        print(request.POST['Voltage'])
+        v=request.POST['Voltage']
     else:
-        print(request.GET['Voltage'])
+        v=request.GET['Voltage']
+    Voltage.objects.create(voltage=v)
     return Response("GOT DATA")
 
 
 @api_view(['GET'])
 def sendVoltage(request):
-    num = round(random.uniform(0, 10), 2)
-    return Response(num)
+    # num = round(random.uniform(0, 10), 2)
+    v=Voltage.objects.latest('time')
+    # v=1
+    return Response({'volt':v.voltage})
 
 
 @api_view(['GET'])
@@ -25,4 +29,5 @@ def sendPrediction(request):
     city = request.GET['city']
     parameters = WeatherForecast.get_weather(city)
     predicted_wattage = Prediction.predict_solar_power(parameters)
+
     return Response(predicted_wattage)
