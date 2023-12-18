2969c8ab6f36f1b6c3f12e13b92bdde187179578
manu00-droid <manavsidana00@gmail.com>
2023-12-18 08:37:36 +0530

    fix: fixed path issues in loading .joblib file

--- TestProj/TestProj/settings.py
+++ TestProj/TestProj/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import sys
 from pathlib import Path
-import os,sys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, os.path.join(BASE_DIR,"APP/Modules"))
+sys.path.insert(0, os.path.abspath(os.path.join(BASE_DIR, "APP")))
+sys.path.insert(0, os.path.abspath(os.path.join(BASE_DIR, "APP/Modules")))
 
 
 # Quick-start development settings - unsuitable for production
